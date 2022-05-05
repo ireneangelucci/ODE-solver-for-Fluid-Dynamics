67c85a06c456bb16b55293c42247e42a9a6e7974
Manish Mishra <manishk.mishra@tum.de>
2022-05-05 15:45:03 +0200

    Using neighbour()

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -11,22 +11,22 @@ void FixedWallBoundary::apply(Fields &field) {
     for(const auto c : _cells){
         if(c->is_border(border_position::TOP)){ //Bottom cells
             field.setv(c->i(),c->j(),0.0);
-            field.setu(c->i(),c->j(),-(field.u(c->i(),(c->j())+1)));
-            field.setp(c->i(),c->j(),field.p(c->i(),(c->j())+1));
+            field.setu(c->i(),c->j(),-(field.u(c->neighbour(border_position::TOP)->i(),c->neighbour(border_position::TOP)->j())));
+            field.setp(c->i(),c->j(),field.p(c->neighbour(border_position::TOP)->i(),c->neighbour(border_position::TOP)->j()));
             field.setg(c->i(),c->j(),field.v(c->i(),c->j()));
         }
         
         if(c->is_border(border_position::RIGHT)){   //Left cells
             field.setu(c->i(),c->j(),0.0);
-            field.setv(c->i(),c->j(),-(field.v(c->i()+1,c->j())));
-            field.setp(c->i(),c->j(),field.p(c->i()+1,c->j()));
+            field.setv(c->i(),c->j(),-(field.v(c->neighbour(border_position::RIGHT)->i(),c->neighbour(border_position::RIGHT)->j())));
+            field.setp(c->i(),c->j(),field.p(c->neighbour(border_position::RIGHT)->i(),c->neighbour(border_position::RIGHT)->j()));
             field.setf(c->i(),c->j(),field.u(c->i(),c->j()));
         }       
         if(c->is_border(border_position::LEFT)){    // Right cells
-            field.setu(c->i()-1,c->j(),0.0);
-            field.setv(c->i(),c->j(),-(field.v(c->i()-1,c->j())));
-            field.setp(c->i(),c->j(),field.p(c->i()-1,c->j()));
-            field.setf(c->i()-1,c->j(),field.u(c->i()-1,c->j()));
+            field.setu(c->neighbour(border_position::LEFT)->i(),c->neighbour(border_position::LEFT)->j(),0.0);
+            field.setv(c->i(),c->j(),-(field.v(c->neighbour(border_position::LEFT)->i(),c->neighbour(border_position::LEFT)->j())));
+            field.setp(c->i(),c->j(),field.p(c->neighbour(border_position::LEFT)->i(),c->neighbour(border_position::LEFT)->j()));
+            field.setf(c->neighbour(border_position::LEFT)->i(),c->neighbour(border_position::LEFT)->j(),field.u(c->neighbour(border_position::LEFT)->i(),c->neighbour(border_position::LEFT)->j()));
         }
     }
 }
@@ -42,10 +42,10 @@ MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, std::map<int, 
 void MovingWallBoundary::apply(Fields &field) {
     for(const auto c : _cells){
         if(c->is_border(border_position::BOTTOM)){
-            field.setv(c->i(),c->j()-1,0.0);
-            field.setu(c->i(),c->j(),2*_wall_velocity.at(c->wall_id())-(field.u(c->i(),(c->j())-1)));
-            field.setp(c->i(),c->j(),field.p(c->i(),(c->j())-1));
-            field.setg(c->i(),c->j()-1,field.v(c->i(),c->j()-1));
+            field.setv(c->neighbour(border_position::BOTTOM)->i(),c->neighbour(border_position::BOTTOM)->j(),0.0);
+            field.setu(c->i(),c->j(),2*_wall_velocity.at(c->wall_id())-(field.u(c->neighbour(border_position::BOTTOM)->i(),c->neighbour(border_position::BOTTOM)->j())));
+            field.setp(c->i(),c->j(),field.p(c->neighbour(border_position::BOTTOM)->i(),c->neighbour(border_position::BOTTOM)->j()));
+            field.setg(c->neighbour(border_position::BOTTOM)->i(),c->neighbour(border_position::BOTTOM)->j(),field.v(c->neighbour(border_position::BOTTOM)->i(),c->neighbour(border_position::BOTTOM)->j()));
         }
     }
 }
